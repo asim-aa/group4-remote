7366ba8585c3e952478a979b672e4a6527719f29
yal137 <yal137@ucsd.edu>
2025-07-09 12:55:09 -0700

    Update utf8analyzer.c

--- utf8analyzer.c
+++ utf8analyzer.c
@@ -10,5 +10,28 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    // implement the UTF-8 analyzer here
+//function 1
+int checkIfASCII(char s[]) {
+        int i = 0;
+        while (s[i] != '\0') {
+            if ((unsigned char)s[i] > 127) {
+                return 0; //fail
+            }
+            i++;
+        }
+        return 1;
+    }
+
+//function 2
+
+int capitalize_ascii(char str[]) {
+        int count = 0;
+    for (int i = 0; str[i] != '\0'; i++) {
+        if (str[i] >= 'a' && str[i] <= 'z') {
+            str[i] -= 32;  
+            count++;
+        }
+    }
+    return count;
 }
+
